3f3e8f6d4d33831493b68df19030d912c4014e9e
Arun <arunappstonelab@gmail.com>
2021-02-05 18:26:17 +0530

    release mode print restriction added

--- ASL_Auth/lib/api_call.dart
+++ ASL_Auth/lib/api_call.dart
@@ -145,7 +145,7 @@ class ApiCall {
 
   // API info...
   static void _printApiDetial(APIRequestInfoObj info) {
-    if (!kReleaseMode) return;
+    if (kReleaseMode) return;
     String apiLog = """
 
         ${info.serviceName} Service Parameters
@@ -161,7 +161,7 @@ class ApiCall {
 
   // API resposne info...
   static void _printResponse(http.Response response, String serviceName) {
-    if (!kReleaseMode) return;
+    if (kReleaseMode) return;
     String apiLog = """
 
         $serviceName Service Response
